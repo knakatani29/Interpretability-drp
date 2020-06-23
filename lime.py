f5b1d1850e7dc400975e9a0e84006d9b01c0bee6
h205c <h205c@smith.cs.haverford.edu>
2020-06-23 10:35:42 -0400

    Commit

--- lime.py
+++ lime.py
@@ -1,14 +1,12 @@
-import os
 import pickle
-
-import numpy as np
 from KNN import ActiveKNN
-from modAL.models import ActiveLearner
-from sklearn.metrics import confusion_matrix
-from sklearn.neighbors import KNeighborsClassifier
 
-infile = open("KNN_few_shot/option_2/KNN_20_shot_2_way_option_2_BAMDIFIROXTEEM-UHFFFAOYSA-N/KNN_BAMDIFIROXTEEM-UHFFFAOYSA-N_option_2.pkl", "rb")
-model = pickle.load(infile)
+import lime
 
+#Unpickling the model
+infile = open("KNN_20_shot_2_way_option_2_BAMDIFIROXTEEM-UHFFFAOYSA-N/KNN_BAMDIFIROXTEEM-UHFFFAOYSA-N_option_2.pkl", "rb")
+activeKNN = pickle.load(infile)
 
+model = activeKNN.model
+amine = activeKNN.amine
 
